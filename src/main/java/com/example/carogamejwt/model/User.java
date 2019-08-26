da64a84ac393f8672bd195cd52d4dfee6026a944
datquocngo <shaco14091999@gmail.com>
2019-08-26 15:59:46 +0700

    make it better

--- src/main/java/com/example/carogamejwt/model/User.java
+++ src/main/java/com/example/carogamejwt/model/User.java
@@ -1,11 +1,9 @@
 package com.example.carogamejwt.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="caro_user")
 public class User {
 
     @Id
